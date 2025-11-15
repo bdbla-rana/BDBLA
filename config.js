2b909217c828d6272e44f08fe4a00813592af930
bdbla-rana <mdrana25080@gmail.com>
2025-11-16 00:35:14 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const GITHUB_USERNAME = "bdbla-rana";
 const REPO_NAME = "BDBLA";
-const TOKEN = "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"; // ★★★ FIX: Update this with your new token ★★★
+const TOKEN = "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"; // ★★★ FIX: Update this with your new token ★★★
 const FOLDER_PATH = "files"; // Folder for history.html
