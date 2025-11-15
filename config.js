0c0f9fd6457c9e90506d2f38c6d058b6f1cdf03d
bdbla-rana <mdrana25080@gmail.com>
2025-11-16 00:09:26 +0600

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// ⚠️ WARNING: This file is PUBLIC and visible in the browser.
+// Do not use this token for a public website.
+
+const GITHUB_USERNAME = "bdbla-rana";
+const REPO_NAME = "BDBLA";
+const TOKEN = "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";
+const FOLDER_PATH = "files"; // Folder for history.html
